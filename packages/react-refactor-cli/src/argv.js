85f6131c4763fcf703269f35fa4e5778310823cd
Christian Vadalà <chr.vadala@gmail.com>
2017-11-26 23:09:30 +0100

    improves argv

--- packages/react-refactor-cli/src/argv.js
+++ packages/react-refactor-cli/src/argv.js
@@ -1,14 +1,15 @@
 const yargs = require('yargs')
-  .usage('Usage: react-refactor [args] <filename>')
+  .usage('Usage: $0 [args] <filename>')
   .help('help')
   .alias('help', 'h')
   .version()
 
-  .option('clipboard', {
-    alias: 'c',
-    describe: 'Copy output on clipboard',
-    type: 'boolean'
-  })
+  //INTO ROADMAP
+  // .option('clipboard', {
+  //   alias: 'c',
+  //   describe: 'Copy output on clipboard',
+  //   type: 'boolean'
+  // })
 
   .option('output', {
     alias: 'o',
