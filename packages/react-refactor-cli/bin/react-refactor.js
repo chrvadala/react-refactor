07660d378aa4c93f71465b00bf07bbc3742d7001
Christian Vadalà <chr.vadala@gmail.com>
2017-11-26 23:06:59 +0100

    adds update notifier

--- packages/react-refactor-cli/bin/react-refactor.js
+++ packages/react-refactor-cli/bin/react-refactor.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+
+//version check
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+updateNotifier({pkg}).notify();
+
+//command
 const argv = require('../build/argv').default
 const command = require('../build/command').default
-
 command(argv._[0], argv)
